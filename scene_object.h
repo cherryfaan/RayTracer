6d5d8252f46474af1f8994e4cee5dc1b10ee2bfc
cherryfaan <xinying.fan@mail.utoronto.ca>
2015-04-01 16:39:29 -0400

    Update scene_object.h
    add cylinder in header

--- scene_object.h
+++ scene_object.h
@@ -34,3 +34,9 @@ public:
 			const Matrix4x4& modelToWorld );
 };
 
+class Cylinder : public SceneObject {
+public:
+        bool intersect( Ray3D& ray, const Matrix4x4& worldToModel,
+                        const Matrix4x4& modelToWorld );
+};
+
